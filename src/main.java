5c0e67e6c85e1401547af27d5c16b579478e8c77
xinglongma <xinglong.ma@hotmail.co.uk>
2013-06-22 00:24:40 +0100

    First commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-6-21
+ * Time: 下午11:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+    public static void main (String [] args){
+        System.out.println("hello world!");
+    }
+}
